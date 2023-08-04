29248b6f31793280c2724d8c7e73706187678354
SOUnit <workbookwork7@gmail.com>
2023-08-03 18:37:38 -0700

    add challenge

---
+++ 01-two-lists/script.js
@@ -0,0 +1,102 @@
+// - 2 list
+// - have to save
+
+var product = {
+  productOptions: [
+    {
+      id: Math.random(),
+      optionItems: [
+        {
+          id: Math.random(),
+          subOptions: [
+            {
+              id: Math.random(),
+              subOptionItems: [
+                { id: Math.random() },
+                { id: Math.random() },
+                { id: Math.random() },
+              ],
+            },
+            {
+              id: Math.random(),
+              subOptionItems: [
+                { id: Math.random() },
+                { id: Math.random() },
+                { id: Math.random() },
+              ],
+            },
+            {
+              id: Math.random(),
+              subOptionItems: [
+                { id: Math.random() },
+                { id: Math.random() },
+                { id: Math.random() },
+              ],
+            },
+          ],
+        },
+        {
+          id: Math.random(),
+          subOptions: [
+            {
+              id: Math.random(),
+              subOptionItems: [
+                { id: Math.random() },
+                { id: Math.random() },
+                { id: Math.random() },
+              ],
+            },
+            {
+              id: Math.random(),
+              subOptionItems: [
+                { id: Math.random() },
+                { id: Math.random() },
+                { id: Math.random() },
+              ],
+            },
+            {
+              id: Math.random(),
+              subOptionItems: [
+                { id: Math.random() },
+                { id: Math.random() },
+                { id: Math.random() },
+              ],
+            },
+          ],
+        },
+        ,
+        {
+          id: Math.random(),
+          subOptions: [
+            {
+              id: Math.random(),
+              subOptionItems: [
+                { id: Math.random() },
+                { id: Math.random() },
+                { id: Math.random() },
+              ],
+            },
+            {
+              id: Math.random(),
+              subOptionItems: [
+                { id: Math.random() },
+                { id: Math.random() },
+                { id: Math.random() },
+              ],
+            },
+            {
+              id: Math.random(),
+              subOptionItems: [
+                { id: Math.random() },
+                { id: Math.random() },
+                { id: Math.random() },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+console.log(product);
