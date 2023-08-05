cff940313b0d3391907c6cdf94c23e1846f756b6
SOUnit <workbookwork7@gmail.com>
2023-08-05 11:49:00 -0700

    fix library error

--- 01-categories/05-add-two-numbers/answer.mjs
+++ 01-categories/05-add-two-numbers/answer.mjs
@@ -1,5 +1,33 @@
 export function answer(l1, l2) {
   console.log({ l1, l2 });
 
-  return null;
+  var num1 = convertToNumber(l1);
+
+  var result = num1;
+
+  console.log({ result });
+
+  return result;
 }
+
+function convertToNumber(linkedListNode) {
+  // num342 -> list243
+  console.log({ linkedListNode });
+
+  // get last, 2
+  var char = `${linkedListNode.val}`;
+  console.log({ char });
+
+  // get next
+  var currentNode = linkedListNode;
+  while (linkedListNode.next) {
+    var nextNode = currentNode.next;
+    char += nextNode.val;
+
+    currentNode = nextNode;
+  }
+
+  return char;
+}
+
+function convertToReverseLinkedList(params) {}
