900c6b867e1301feb51123779b9d51670c5ce33b
SOUnit <workbookwork7@gmail.com>
2023-08-05 10:57:47 -0700

    add tests

---
+++ 01-categories/05-add-two-numbers/quiz.mjs
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+import { test } from "../../lib/utils.mjs";
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  // var res1 = getNumberFromLinkedList(l1);
+  // console.log({ res1, res2 });
+  test("This is test!!!");
+};
+
+// create test data
+
+// execute
+addTwoNumbers();
