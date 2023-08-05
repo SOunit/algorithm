7f18af92773e10631b522dc25b033dc83802e0db
SOUnit <workbookwork7@gmail.com>
2023-08-05 11:34:42 -0700

    finish setup quiz

---
+++ 01-categories/05-add-two-numbers/answer.mjs
@@ -0,0 +1,5 @@
+export function answer(l1, l2) {
+  console.log({ l1, l2 });
+
+  return null;
+}
--- 01-categories/05-add-two-numbers/quiz.mjs
+++ 01-categories/05-add-two-numbers/quiz.mjs
@@ -7,6 +7,7 @@
  */
 
 import { convertNumberToReverseLinkedList } from "../../lib/utils.mjs";
+import { answer } from "./answer.mjs";
 
 /**
  * @param {ListNode} l1
@@ -14,13 +15,12 @@ import { convertNumberToReverseLinkedList } from "../../lib/utils.mjs";
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  // var res1 = getNumberFromLinkedList(l1);
-  // console.log({ res1, res2 });
-
-  convertNumberToReverseLinkedList(100);
+  return answer(l1, l2);
 };
 
 // create test data
+const listNode1 = convertNumberToReverseLinkedList(342);
+const listNode2 = convertNumberToReverseLinkedList(465);
 
 // execute
-addTwoNumbers();
+addTwoNumbers(listNode1, listNode2);
