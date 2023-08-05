55e70bf3cd4315b0e341260281d1023ddbc99ea8
SOUnit <workbookwork7@gmail.com>
2023-08-05 12:01:54 -0700

    add conversion to number

--- 01-categories/05-add-two-numbers/answer.mjs
+++ 01-categories/05-add-two-numbers/answer.mjs
@@ -2,32 +2,39 @@ export function answer(l1, l2) {
   console.log({ l1, l2 });
 
   var num1 = convertToNumber(l1);
+  var num2 = convertToNumber(l2);
+  var sum = num1 + num2;
+  var reversedLinkedList = convertToReverseLinkedList(sum);
 
-  var result = num1;
+  var result = reversedLinkedList;
 
   console.log({ result });
 
   return result;
 }
 
-function convertToNumber(linkedListNode) {
-  // num342 -> list243
-  console.log({ linkedListNode });
+// helper functions
+
+function convertToNumber(reversedLinkedListNode) {
+  // list243 -> num342
+  console.log({ reversedLinkedListNode });
 
   // get last, 2
-  var char = `${linkedListNode.val}`;
+  var char = `${reversedLinkedListNode.val}`;
   console.log({ char });
 
   // get next
-  var currentNode = linkedListNode;
-  while (linkedListNode.next) {
+  var currentNode = reversedLinkedListNode;
+  while (currentNode.next) {
     var nextNode = currentNode.next;
-    char += nextNode.val;
-
+    char = nextNode.val + char;
     currentNode = nextNode;
   }
 
-  return char;
+  return +char;
 }
 
-function convertToReverseLinkedList(params) {}
+function convertToReverseLinkedList(number) {
+  var res = number;
+  return res;
+}
