dc2034487acca43fd7c8f89903f6d4182acc7d85
SOUnit <workbookwork7@gmail.com>
2023-08-08 19:03:47 -0700

    setup quiz

---
+++ 01-categories/08-kth-largest/quiz.mjs
@@ -0,0 +1,41 @@
+import { add, createClass } from "./answer.mjs";
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function (k, nums) {
+  createClass(k, nums);
+};
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function (val) {
+  add(val);
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
+
+// test data
+var k = 3;
+var nums = [4, 5, 8, 2];
+
+// execute
+var obj = new KthLargest(k, nums);
+var param_1 = obj.add(3);
+var param_2 = obj.add(5);
+var param_3 = obj.add(10);
+var param_4 = obj.add(9);
+var param_5 = obj.add(4);
+
+console.log({ expected: 4, val: param_1 });
+console.log({ expected: 5, val: param_2 });
+console.log({ expected: 5, val: param_3 });
+console.log({ expected: 8, val: param_4 });
+console.log({ expected: 8, val: param_5 });
